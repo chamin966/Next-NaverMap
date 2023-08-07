518920fb5cbf0995a6395b0a883e37d8a48c3314
Minchan <98478661+chamin966@users.noreply.github.com>
2023-08-07 15:47:55 +0900

    Feat: 검색창 모듈화

---
+++ my-next-project/components/home/SearchBar.tsx
@@ -0,0 +1,46 @@
+import styles from '../../styles/sideBar.module.scss';
+import useSearchedStore from '@/hooks/useSearchedStore';
+import useCurrentStore from '@/hooks/useCurrentStore';
+import { useState } from 'react';
+
+function SearchBar() {
+  const { setSearchedStore, clearSearchedStore } = useSearchedStore();
+  const { clearCurrentStore } = useCurrentStore();
+
+  const [inputText, setInputText] = useState<string>('');
+
+  const onChangeInputText = (e: React.ChangeEvent<HTMLInputElement>) => {
+    return setInputText(e.target.value);
+  };
+
+  const onSubmitInputText = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setInputText('');
+    const stores = (await import('../../public/stores.json')).default;
+    const store = stores.find((store) => store.name === inputText);
+    if (store) setSearchedStore(store);
+  };
+
+  const onClickErase = () => {
+    setInputText('');
+    clearSearchedStore();
+    clearCurrentStore();
+  };
+
+  return (
+    <>
+      <form onSubmit={onSubmitInputText}>
+        <input
+          type="text"
+          className={styles.SearchBar}
+          placeholder="식당 검색"
+          value={inputText}
+          onChange={onChangeInputText}
+        />
+      </form>
+      <button onClick={onClickErase}>X</button>
+    </>
+  );
+}
+
+export default SearchBar;
--- my-next-project/components/home/SideBarSection.tsx
+++ my-next-project/components/home/SideBarSection.tsx
@@ -5,47 +5,16 @@ import DetailSection from './DetailSection';
 import React, { useState } from 'react';
 import useSwr from 'swr';
 import { Store } from '@/types/store';
-import useSearchedStore, { SEARCHED_STORE_KEY } from '@/hooks/useSearchedStore';
-import useCurrentStore from '@/hooks/useCurrentStore';
+import { SEARCHED_STORE_KEY } from '@/hooks/useSearchedStore';
+import SearchBar from './SearchBar';
 
 function SideBarSection() {
   const { data: searchedStore } = useSwr<Store>(SEARCHED_STORE_KEY);
-  const { setSearchedStore, clearSearchedStore } = useSearchedStore();
-  const { clearCurrentStore } = useCurrentStore();
-
-  const [inputText, setInputText] = useState<string>('');
-
-  const onChangeInputText = (e: React.ChangeEvent<HTMLInputElement>) => {
-    return setInputText(e.target.value);
-  };
-
-  const onSubmitInputText = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    setInputText('');
-    const stores = (await import('../../public/stores.json')).default;
-    const store = stores.find((store) => store.name === inputText);
-    if (store) setSearchedStore(store);
-  };
-
-  const onClickErase = () => {
-    setInputText('');
-    clearSearchedStore();
-    clearCurrentStore();
-  };
 
   return (
     <div className={styles.SideBarContainer}>
       <div className={styles.SearchContainer}>
-        <form onSubmit={onSubmitInputText}>
-          <input
-            type="text"
-            className={styles.SearchBar}
-            placeholder="식당 검색"
-            value={inputText}
-            onChange={onChangeInputText}
-          />
-        </form>
-        <button onClick={onClickErase}>X</button>
+        <SearchBar />
         <StoresSection />
         <div className={styles.FoldingButton}>
           <LiaAngleRightSolid size={20}>접기</LiaAngleRightSolid>
